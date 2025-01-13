fb5166ccbac4565d2149fd7752997f2eca02cbad
Ángel Roldán Rabanal <aroldanrabanal@safareyes.es>
2025-01-13 08:58:31 +0100

    java

---
+++ java/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // constructor
+        // nombreInterface nombre=new nombreClase();
+
+        System.out.println("Hola mundo");
+    }
+}
+
