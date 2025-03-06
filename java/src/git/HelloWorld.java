82e5d7b24b3cb20202d0f05c46560b1c92936bb3
Ángel Roldán Rabanal <aroldanrabanal@safareyes.es>
2025-03-06 13:51:30 +0100

    Modificado HelloWorld.java mostrando tambien fecha del sistema

--- java/src/git/HelloWorld.java
+++ java/src/git/HelloWorld.java
@@ -1,7 +1,15 @@
 package git;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedDateTime = now.format(formatter);
+
         System.out.println("Hello, World!");
+        System.out.println("La fecha actual es: " + formattedDateTime);
     }
 }
